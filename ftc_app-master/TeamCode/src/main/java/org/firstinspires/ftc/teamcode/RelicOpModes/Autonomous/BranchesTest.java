625c5dd0c5c11eb9da5c5285ce92493030fd32fb
Isaac Blandin Mac <myung0966@gmail.com>
2017-11-01 19:21:06 -0500

    started an opmode to test init stage user input

---
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RelicOpModes/Autonomous/BranchesTest.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode.RelicOpModes.Autonomous;
+
+import org.firstinspires.ftc.teamcode.RelicRecoveryRobotBase.RelicAutonomousBase;
+import org.firstinspires.ftc.teamcode.enums.RobotRunType;
+
+/**
+ * Created by isaac.blandin on 11/1/17.
+ */
+
+public class BranchesTest extends RelicAutonomousBase {
+
+public void runOpMode() {
+    initRobot(RobotRunType.AUTONOMOUS);
+
+    waitForStart();
+}
+}

eee2c00e94ee83cf0bbc961767f34875e27bd9cf
Isaac Blandin Mac <myung0966@gmail.com>
2017-11-06 09:43:59 -0600

    moved vumark test to the tests project folder

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RelicOpModes/Tests/VuMarkTest.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RelicOpModes/Tests/VuMarkTest.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.RelicOpModes.TeleOp;
+package org.firstinspires.ftc.teamcode.RelicOpModes.Tests;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;

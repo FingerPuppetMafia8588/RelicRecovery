677156e07888b024b35a1e4a1a1a4f75262b777b
Isaac Blandin Mac <myung0966@gmail.com>
2017-11-18 11:22:17 -0600

    added telemetry and comments

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RelicOpModes/positioning/armsDown.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RelicOpModes/positioning/armsDown.java
@@ -14,27 +14,41 @@ public class armsDown extends RelicHardware {
 
     public void runOpMode() {
 
+        //initialize hardware
         initRobot(RobotRunType.AUTONOMOUS);
 
+        //set variables to full down
         double right = 1;
         double left = 0;
 
         waitForStart();
 
+        //put arms to full down position
         JewelRight.setPosition(1);
         JewelLeft.setPosition(0);
 
         while (opModeIsActive()) {
+
+            //raise arms if a is pressed
             if (gamepad1.a) {
                 right -= 0.1;
                 left += 0.1;
-            } else if (gamepad1.b) {
+                sleep(100);
+            } //lower arm if b is pressed
+            else if (gamepad1.b) {
                 right += 0.1;
                 left -= 0.1;
+                sleep(100);
             }
 
+            //assign variables to servo position
             JewelRight.setPosition(right);
             JewelLeft.setPosition(left);
+
+            //give feedback to the Driver Station
+            telemetry.addData("Right", JewelRight.getPosition());
+            telemetry.addData("Left", JewelLeft.getPosition());
+            telemetry.update();
         }
     }
 }

4345067934fe24e90f3555ae4db2bee9918ae663
Isaac Blandin Mac <myung0966@gmail.com>
2017-12-10 15:13:57 -0600

    added opmode to test enabling color sensor leds

---
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RelicOpModes/Tests/ColorLED.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode.RelicOpModes.Tests;
+
+import org.firstinspires.ftc.teamcode.RelicRecoveryRobotBase.RelicHardware;
+
+/**
+ * Created by isaac.blandin on 12/10/17.
+ */
+
+public class ColorLED extends RelicHardware {
+
+    public void runOpMode() {
+
+    }
+}

c532b4c607ee2a4777f1ce959228416159abae53
Isaac Blandin Mac <myung0966@gmail.com>
2017-12-12 08:15:34 -0600

    converted theta range from -pi to pi to 0 to 0 to 2pi

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RelicOpModes/Tests/FieldCentric.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RelicOpModes/Tests/FieldCentric.java
@@ -50,7 +50,7 @@ public class FieldCentric extends RelicHardware {
             right = gamepad1.left_stick_x;
             clockwise = gamepad1.right_stick_x;
 
-            theta = imu.getIntegratedZValue()/180*Math.PI;
+            theta = imu.getIntegratedZValue()/180*Math.PI + Math.PI;
 
             //reset variables
             frontRight = 0;
